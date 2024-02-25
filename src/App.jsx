dd923a5896e0e7e747738c654274bf97790cbb29
Hamil <dimapanat.hamil@gmail.com>
2024-02-25 16:20:30 -0500

    added drag and drop functionality with react-dropzone

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 import OpenAI from "openai";
 import ReactMarkdown from "react-markdown";
+import { useDropzone } from "react-dropzone";
 import "./App.css";
-
-
+import { recipe } from "./recipe";
 const App = () => {
   const [response, setResponse] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [imageUrl, setImageUrl] = useState("");
+  const [preview, setPreview] = useState(null);
+  const [fileUploaded, setFileUploaded] = useState(false);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = new FileReader();
+    file.onload = () => {
+      setPreview(file.result);
+      setFileUploaded(true);
+    };
+    console.log(acceptedFiles[0]);
+    file.readAsDataURL(acceptedFiles[0]);
+
+  }, []);
+
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive } =
+    useDropzone({
+      onDrop,
+      accept: {
+        "image/jpeg": [],
+        "image/png": [],
+      },
+    });
 
   const openai = new OpenAI({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true
+    dangerouslyAllowBrowser: true,
   });
 
-  const fetchUIResponse = async () => {
+  const fetchUIResponse = async (imageUrl) => {
     setIsLoading(true);
     try {
       const result = await openai.chat.completions.create({
@@ -23,20 +44,29 @@ const App = () => {
           {
             role: "user",
             content: [
-              { type: "text", text: "You are a professional chef. Based on the ingredients in the picture, suggest a dish that I can make. List ingredients with (-) and instructions with numbers (ex: 1. 2. 3.)" },
+              {
+                type: "text",
+                text: "You are a professional chef. Based on the ingredients in the picture, suggest a dish that I can make. List ingredients with (-) and instructions with numbers (ex: 1. 2. 3.)",
+              },
               {
                 type: "image_url",
                 image_url: {
-                  "url": imageUrl,
+                  url: imageUrl,
                 },
               },
             ],
           },
         ],
-        "max_tokens": 1500
+        max_tokens: 1500,
       });
-      if (result && result.choices && result.choices.length > 0 && result.choices[0].message) {
+      if (
+        result &&
+        result.choices &&
+        result.choices.length > 0 &&
+        result.choices[0].message
+      ) {
         setResponse(result.choices[0].message.content);
+        setImageUrl("");
         console.log(response);
       }
     } catch (error) {
@@ -46,30 +76,56 @@ const App = () => {
     }
   };
 
-  const handleImageLinkChange = (event) => {
-    setImageUrl(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    fetchUIResponse();
-    setImageUrl('')
-  };
+    if (typeof acceptedFiles[0] === "undefined") return;
+    const formData = new FormData();
+    formData.append("file", acceptedFiles[0]);
+    formData.append("upload_preset", "tde7u3xk");
+    formData.append("api_key", import.meta.env.VITE_CLOUDINARY_API_KEY);
 
+    const results = await fetch(
+      "https://api.cloudinary.com/v1_1/dvr6suq9d/image/upload",
+      {
+        method: "POST",
+        body: formData,
+      }
+    ).then((res) => res.json());
+    console.log(results);
+    fetchUIResponse(results.secure_url);
+  };
   return (
     <div>
-      <div>
-      <h3 className="header">Hi! UI Expert Here</h3>
-
+      <div className="head">
+        <h3 className="header">Flavor Frames</h3>
+        <div className="description-box">
+          <div></div>
+          <div>
+            <h5 className="description">
+              A web application that leverages the power of OpenAI GPT-4 Vision
+              to transform images of ingredients into creative and delicious
+              recipes. Whether you're a professional chef seeking inspiration or
+              a home cook looking to explore new culinary horizons, Flavor
+              Frames is designed to spark your imagination and guide you through
+              the art of cooking with AI.
+            </h5>
+          </div>
+          <div></div>
+        </div>
       </div>
-      
+
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={imageUrl}
-          onChange={handleImageLinkChange}
-          placeholder="Paste the image link here"
-        />
+        {!fileUploaded && (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>Drop the files here ...</p>
+            ) : (
+              <p className="drop-text">Drag 'n' drop some files here, or click to select files</p>
+            )}
+          </div>
+        )}
+        <img src={preview} alt="" />
         <button type="submit">Submit</button>
       </form>
       {isLoading ? (
@@ -79,10 +135,7 @@ const App = () => {
           <h3 className="ai-feedback">My Feedback:</h3>
           <ReactMarkdown>{response}</ReactMarkdown>
         </div>
-      ) : (
-
-        <p className="no-response">No response received.</p>
-      )}
+      ) : null}
     </div>
   );
 };
---
+++ src/recipe.js
@@ -0,0 +1,24 @@
+export const recipe = `MY FEEDBACK:
+Certainly! Based on the ingredients in the picture, you could make a classic Carbonara pasta. Here's a suggested recipe:
+
+Ingredients:
+
+-Spaghetti or your choice of long pasta
+-Diced pancetta or guanciale (Italian cured meat from pork cheek or jowl)
+-Freshly grated Parmesan cheese
+-Eggs (4 eggs - ideally, use 2 whole eggs and 2 egg yolks for extra creaminess)
+-Finely chopped or minced garlic
+-Extra virgin olive oil
+-Salt
+-Freshly ground black pepper
+Instructions:
+
+1. Bring a large pot of salted water to a boil and cook the pasta according to package instructions until al dente.
+2. While the pasta is cooking, heat a large skillet over medium heat and add a splash of olive oil.
+3. Add the pancetta or guanciale to the skillet and sauté until it starts to become crispy. Add the garlic and cook for an additional minute until fragrant. Be careful not to burn the garlic.
+4. In a bowl, mix together the eggs, Parmesan cheese, a pinch of salt, and a generous amount of freshly ground black pepper. Whisk until well combined.
+5. Once the pasta is cooked, reserve a cup of the pasta water and then drain the pasta.
+6. With the skillet off the heat, add the hot pasta to the pancetta/garlic mixture and mix well to coat the pasta in the fats.
+7. Quickly pour the egg and cheese mixture into the pasta, and using tongs or a fork, toss everything together to combine. The heat from the pasta will gently cook the eggs to create a creamy sauce. If it's too thick, add a splash of the reserved pasta water and mix until you reach the desired consistency.
+8. Serve immediately, with additional grated Parmesan and a twist of black pepper on top.
+Enjoy your delicious homemade Carbonara!`
